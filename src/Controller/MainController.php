8d2439ba72695240631e94d53b4432d65a01aad7
BERTHIER <contact@daniel-dev.me>
2022-02-01 16:20:06 +0100

    :sparkles: create database and save datas

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Argonaute;
+use App\Form\ArgonauteType;
+use App\Repository\ArgonauteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +16,59 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, ArgonauteRepository $argoRepo): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        $argonautes = $argoRepo->findAll();
+        $argonaute = new Argonaute();
+        $form = $this->createForm(ArgonauteType::class, $argonaute);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($argonaute);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('main/index.html.twig', [
+            'argonaute' => $argonaute,
+            'argonautes' => $argonautes,
+            'form' => $form,
         ]);
     }
+
+     /**
+     * @Route("/{id}", name="delete", methods={"POST"})
+     */
+    public function delete($id, Request $request, Argonaute $argonaute, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$argonaute->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($argonaute);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
+    }
+
+     /**
+     * @Route("/{id}/edit", name="edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Argonaute $argonaute, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArgonauteType::class, $argonaute);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('main/edit.html.twig', [
+            'argonaute' => $argonaute,
+            'form' => $form,
+        ]);
+    }
+
+
 }
